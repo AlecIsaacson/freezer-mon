9b250cf66f5e22e1ebcb8319b20314ed5e6a94f9
AlecIsaacson <aisaacson@newrelic.com>
2022-02-27 14:26:58 -0500

    Another update to handle sensor readout

--- getTemp.sh
+++ getTemp.sh
@@ -4,12 +4,16 @@ loopCount=0
 
 while [ -z $temp ]; do
    let loopCount+=1
-   temp=`cat /root/$1 | grep -Po 't=\K(.*)$'`
+   rawTemp=`cat /root/$1`
+   temp=`echo $rawTemp | grep -Po 't=\K(.*)$'`
    sleep 3
 done
 
 if [ $loopCount -gt 3 ]; then 
-   logger -p local0.notice -t GET-TEMP "Get temp for $1 took $loopCount loops. Result was $temp"
+   logger -p local0.notice -t GET-TEMP "Get temp for $1 took $loopCount loops. Result was $temp. Raw result: $rawTemp"
 fi
+
+echo "`date` - $1 - $loopCount - $rawTemp" >> /var/lib/rpimonitor/stat/getTemp.out
+
 
 echo "$temp"
